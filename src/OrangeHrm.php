711386a1ffa01cd66413f26498b7321e9db9a70f
grantholle <grantholle@users.noreply.github.com>
2021-08-25 02:55:36 +0000

    Fix styling

--- src/OrangeHrm.php
+++ src/OrangeHrm.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Client\PendingRequest;
 use Illuminate\Http\Client\Response;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
 
 class OrangeHrm
 {
@@ -14,7 +13,7 @@ class OrangeHrm
     protected string $clientId;
     protected string $clientSecret;
     protected ?string $accessToken;
-    const CACHE_KEY = 'orangehrm_access_token';
+    public const CACHE_KEY = 'orangehrm_access_token';
 
     public function __construct(string $url, string $clientId, string $clientSecret)
     {
@@ -31,7 +30,7 @@ class OrangeHrm
 
     protected function setAccessToken(bool $force = false): void
     {
-        if (!$force && $this->accessToken) {
+        if (! $force && $this->accessToken) {
             return;
         }
 
@@ -49,7 +48,7 @@ class OrangeHrm
 
     protected function http(): PendingRequest
     {
-        if (!$this->accessToken) {
+        if (! $this->accessToken) {
             $this->setAccessToken(true);
         }
 
@@ -86,7 +85,7 @@ class OrangeHrm
         ])->json();
     }
 
-    public function getEmployeeCustomFields($id, ?string $screen = 'personal', ?string $module = 'pim'):? array
+    public function getEmployeeCustomFields($id, ?string $screen = 'personal', ?string $module = 'pim'): ?array
     {
         return $this->http()
             ->get("/api/employees/{$id}/CustomFieldValue?filter[screen]={$screen}&filter[screen][module]={$module}")
